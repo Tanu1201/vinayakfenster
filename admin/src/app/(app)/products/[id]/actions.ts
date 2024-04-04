59422dcfb53d6aa9e08abc3fe43ad6ebbe922589
jkugsiya <jkugsiya11@gmail.com>
2024-04-04 22:17:24 +0530

    fix: removed unused imports

--- admin/src/app/(app)/products/[id]/actions.ts
+++ admin/src/app/(app)/products/[id]/actions.ts
@@ -1,39 +1,39 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
+import { revalidatePath } from "next/cache";
 
-import { getAuthSession } from '@/lib/auth'
-import { prisma } from '@/lib/db'
-import { storageClient } from '@/lib/storageClient'
-import { UnwrapPromise } from '@/types/UnwrapPromise'
+import { getAuthSession } from "@/lib/auth";
+import { prisma } from "@/lib/db";
+import { storageClient } from "@/lib/storageClient";
+import { UnwrapPromise } from "@/types/UnwrapPromise";
 
 export const getProduct = async (id: string) => {
-  const session = await getAuthSession()
-  if (!session) throw new Error('Unauthorized')
+  const session = await getAuthSession();
+  if (!session) throw new Error("Unauthorized");
 
   return await prisma.product.findUnique({
     where: {
-      id
+      id,
     },
     include: {
       createdBy: true,
       updatedBy: true,
       productBrands: {
         select: {
-          brand: true
-        }
+          brand: true,
+        },
       },
       productCategories: {
         select: {
-          category: true
-        }
+          category: true,
+        },
       },
-      productImages: true
-    }
-  })
-}
+      productImages: true,
+    },
+  });
+};
 
-export type GetProductFnDataType = UnwrapPromise<ReturnType<typeof getProduct>>
+export type GetProductFnDataType = UnwrapPromise<ReturnType<typeof getProduct>>;
 
 export const createProduct = async ({
   name,
@@ -41,17 +41,17 @@ export const createProduct = async ({
   description,
   brandIds,
   categoryIds,
-  fileIds
+  fileIds,
 }: {
-  name: string
-  slug: string
-  description: any
-  brandIds: string[]
-  categoryIds: string[]
-  fileIds?: string[]
+  name: string;
+  slug: string;
+  description: any;
+  brandIds: string[];
+  categoryIds: string[];
+  fileIds?: string[];
 }) => {
-  const session = await getAuthSession()
-  if (!session) throw new Error('Unauthorized')
+  const session = await getAuthSession();
+  if (!session) throw new Error("Unauthorized");
   const { id } = await prisma.product.create({
     data: {
       name,
@@ -60,35 +60,35 @@ export const createProduct = async ({
       createdById: session.user.id,
       updatedById: session.user.id,
       productBrands: {
-        create: brandIds.map(brandId => ({
-          brandId
-        }))
+        create: brandIds.map((brandId) => ({
+          brandId,
+        })),
       },
       productCategories: {
-        create: categoryIds.map(categoryId => ({
-          categoryId
-        }))
-      }
-    }
-  })
+        create: categoryIds.map((categoryId) => ({
+          categoryId,
+        })),
+      },
+    },
+  });
 
   if (fileIds?.length)
     await prisma.resource.updateMany({
       where: {
         id: {
-          in: fileIds
-        }
+          in: fileIds,
+        },
       },
       data: {
-        productId: id
-      }
-    })
+        productId: id,
+      },
+    });
 
-  revalidatePath('/products')
-  revalidatePath(`/products/${id}`)
+  revalidatePath("/products");
+  revalidatePath(`/products/${id}`);
 
-  return id
-}
+  return id;
+};
 
 export const updateProduct = async ({
   id,
@@ -97,21 +97,21 @@ export const updateProduct = async ({
   description,
   brandIds,
   categoryIds,
-  fileIds
+  fileIds,
 }: {
-  id: string
-  name: string
-  slug: string
-  description: any
-  brandIds: string[]
-  categoryIds: string[]
-  fileIds?: string[]
+  id: string;
+  name: string;
+  slug: string;
+  description: any;
+  brandIds: string[];
+  categoryIds: string[];
+  fileIds?: string[];
 }) => {
-  const session = await getAuthSession()
-  if (!session) throw new Error('Unauthorized')
+  const session = await getAuthSession();
+  if (!session) throw new Error("Unauthorized");
   await prisma.product.update({
     where: {
-      id
+      id,
     },
     data: {
       name,
@@ -120,76 +120,88 @@ export const updateProduct = async ({
       updatedById: session.user.id,
       productBrands: {
         deleteMany: {},
-        create: brandIds.map(brandId => ({
-          brandId
-        }))
+        create: brandIds.map((brandId) => ({
+          brandId,
+        })),
       },
       productCategories: {
         deleteMany: {},
-        create: categoryIds.map(categoryId => ({
-          categoryId
-        }))
-      }
-    }
-  })
+        create: categoryIds.map((categoryId) => ({
+          categoryId,
+        })),
+      },
+    },
+  });
 
   if (fileIds?.length)
     await prisma.resource.updateMany({
       where: {
         id: {
-          in: fileIds
-        }
+          in: fileIds,
+        },
       },
       data: {
-        productId: id
-      }
-    })
+        productId: id,
+      },
+    });
 
-  revalidatePath('/products')
-  revalidatePath(`/products/${id}`)
-}
+  revalidatePath("/products");
+  revalidatePath(`/products/${id}`);
+};
 
 export const deleteProduct = async (id: string) => {
-  const session = await getAuthSession()
-  if (!session) throw new Error('Unauthorized')
+  const session = await getAuthSession();
+  if (!session) throw new Error("Unauthorized");
 
-  await prisma.resource.deleteMany({
-    where: {
-      productId: id
-    }
-  })
+  await Promise.all([
+    prisma.resource.deleteMany({
+      where: {
+        productId: id,
+      },
+    }),
+    prisma.productBrand.deleteMany({
+      where: {
+        productId: id,
+      },
+    }),
+    prisma.productCategory.deleteMany({
+      where: {
+        productId: id,
+      },
+    }),
+  ]);
 
   await prisma.product.delete({
     where: {
-      id
-    }
-  })
+      id,
+    },
+  });
 
-  revalidatePath('/products')
-  revalidatePath(`/products/${id}`)
-}
+  revalidatePath("/products");
+  revalidatePath(`/products/${id}`);
+};
 
 export const deleteProductImage = async (productImageId: string) => {
-  const session = await getAuthSession()
-  if (!session) throw new Error('Unauthorized')
+  const session = await getAuthSession();
+  if (!session) throw new Error("Unauthorized");
   const productImage = await prisma.resource.findUnique({
     where: {
-      id: productImageId
+      id: productImageId,
     },
     include: {
-      product: true
-    }
-  })
-  if (!productImage) throw new Error('Product image not found')
+      product: true,
+    },
+  });
+  if (!productImage) throw new Error("Product image not found");
   await Promise.all([
     storageClient.deleteFile(productImage.newFilename),
     prisma.resource.delete({
       where: {
-        id: productImageId
-      }
-    })
-  ])
+        id: productImageId,
+      },
+    }),
+  ]);
 
-  revalidatePath('/products')
-  revalidatePath(`/products/${productImageId}`)
-}
+  revalidatePath("/products");
+  revalidatePath(`/products/${productImageId}`);
+};
